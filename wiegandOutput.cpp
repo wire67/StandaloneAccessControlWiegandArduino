2c605c4931e3b0ad680d3eb6c577f2a7e0963814
Jerome <j67160@gmail.com>
2024-10-20 13:26:56 +0800

    fixed index calculation

--- wiegandOutput.cpp
+++ wiegandOutput.cpp
@@ -283,30 +283,27 @@ void WiegandOut::writeArray(const uint8_t *myDigits, const uint8_t length)
 
 void WiegandOut::writeString(const char *digitString)
 {
-   for (uint8_t i = 0; i < strlen(digitString); i++)
-   {
-      const char *end = strchr(digitString + i, ']');
-      if ('[' == digitString[i] && end != NULL)
-      {
-         // Calculate the length of the substring up to ']'
-         size_t length = end - digitString;
-         // Allocate memory for the substring (don't forget space for null-terminator)
-         char substring[length + 1];
-         // Copy the substring
-         strncpy(substring, digitString, length);
-         // Null-terminate the substring
-         substring[length] = '\0';
-         unsigned long value = strtoul(substring, NULL, 10);
-         send(value, 26, false);
-      }
-      else
-      {
-         if (writeChar(digitString[i]))
-         {
-            delay(500);
-         }
-      }
-   }
+    for (uint8_t i = 0; i < strlen(digitString); i++)
+    {
+        const char *end = strchr(digitString + i, ']');
+        if ('[' == digitString[i] && end != NULL)
+        {
+            size_t length = end - (digitString + i + 1);
+            char substring[length + 1];
+            strncpy(substring, digitString + i + 1, length);
+            substring[length] = '\0';
+            unsigned long value = strtoul(substring, NULL, 10);
+            send(value, 26, false);
+            i += length + 1;
+        }
+        else
+        {
+            if (writeChar(digitString[i]))
+            {
+                delay(500);
+            }
+        }
+    }
 }
 
 /**
